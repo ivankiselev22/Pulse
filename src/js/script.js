85f9b70a7f17f6dc74ec27151e498d810f9f5c51
Ivan <kiselyow.ivan2014@gmail.com>
2019-11-02 16:49:27 +0300

    mailer

--- src/js/script.js
+++ src/js/script.js
@@ -27,7 +27,7 @@
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass ('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass ('catalog-item__list_active');
-            })
+            });
         });
     };
 
@@ -47,7 +47,7 @@
         $(this).on('click', function() {
             $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
             $('.overlay, #order').fadeIn('slow');
-        })
+        });
     });
 
     function valideForms(form){
@@ -80,6 +80,39 @@
     valideForms('#consultation-form');
     valideForms('#consultation form');
     valideForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "../mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('overlay, #thanks').fadeIn('Slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    //smooth scroll and pageup
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
 
     new WOW().init();
   });
